d41cbc4340bc485c0a11da7dbb2597c9932767c4
zobaidul kazi <zobaidulkazi64.official@gmail.com>
2024-05-28 07:52:01 +0600

    blog and portfollyeo pages test

--- src/app/company/portfolio/page.tsx
+++ src/app/company/portfolio/page.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const portfolioPage = () => {
   return (
-    <div>portfolioPage</div>
-  )
-}
+    <div>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam, nobis
+        nemo ratione quae consequatur id similique molestias! Harum adipisci,
+        quo alias, nobis necessitatibus repudiandae reprehenderit pariatur cum
+        excepturi assumenda beatae.
+      </p>
+    </div>
+  );
+};
 
-export default portfolioPage
+export default portfolioPage;
